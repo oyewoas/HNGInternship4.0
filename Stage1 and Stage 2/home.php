a93f8c3bf41850e0475e5e95715ec14302851e79
OYEWO Ayooluwa <31412106+oyewoas@users.noreply.github.com>
2018-04-13 13:30:44 +0100

    new php code

---
+++ Stage1 and Stage 2/home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="css/home.css">
+        <title>HNGInternship 4.0</title>
+    </head>
+
+    <body>
+
+        <main>
+            <div>
+                <h1>HNGInternship 4.0</h1>
+            </div>
+
+            <p>Now I can Finally LEARN TO CODE<br>Thanks to Hotels.ng<br><?php
+date_default_timezone_set("Africa/Lagos");
+echo "The time is " . date("h:i:sa");
+?></p>
+           
+
+
+        </main>
+        <footer><span>NOW LETS CODE</span></footer>
+
+    </body>
+
+    </html>
