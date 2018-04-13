c716c633b2280d7e4b7c792f14fd97ae99c2d2c3
OYEWO Ayooluwa <31412106+oyewoas@users.noreply.github.com>
2018-04-13 13:25:24 +0000

    Update home.php

--- Stage1 and Stage 2/home.php
+++ Stage1 and Stage 2/home.php
@@ -6,7 +6,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="css/homephp.css">
+        <link rel="stylesheet" href="css/home.css">
         <title>HNGInternship 4.0</title>
     </head>
 
